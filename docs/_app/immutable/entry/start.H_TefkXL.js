cd6ac7d89de6e285194e1ae016e7f03a9eca074b
Michael Adams <mtadams42@gmail.com>
2025-05-07 06:19:11 -0700

    Try to make links be relative to the parent directory

--- docs/_app/immutable/entry/start.H_TefkXL.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DJ3xRnaq.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,9 @@ const config = {
 			pages: "docs",
 			fallback: "404.html", // may differ from host to host
 		}),
+		paths: {
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+		},
 	},
 };
 
