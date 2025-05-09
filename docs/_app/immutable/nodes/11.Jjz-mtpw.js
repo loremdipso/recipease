d51450865e3aacd8fab8f253fdfc52a26b83f574
Michael Adams <mtadams42@gmail.com>
2025-05-09 15:29:16 -0700

    Bulid

--- docs/_app/immutable/nodes/11.Jjz-mtpw.js
+++
@@ -1,3 +0,0 @@
-import{t,a as r}from"../chunks/Djqy1-Es.js";import"../chunks/Bq646O2v.js";import{f as s,v as i}from"../chunks/CH411up-.js";import{T as p}from"../chunks/DfHE-vPG.js";var n=t(`<!> <main><div class="card flex-col gap1"><p>This is still a work in progress. If you found this website at all,
-			well, kudos. I guarantee nothing, I promise nothing, use at your own
-			risk.</p> <p>p.s. please don't sue me</p></div></main>`,1);function c(o){var a=n(),e=s(a);p(e,{title:"Terms of Service",back_path:"/"}),i(2),r(o,a)}export{c as component};
